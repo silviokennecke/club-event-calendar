4686d38df51d8566b4924144e11101e2196fb959
Silvio Kennecke <kennecke@heptacom.de>
2024-01-11 16:33:44 +0100

    change namespace

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace SilvioKennecke\ClubEventCalendar;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
